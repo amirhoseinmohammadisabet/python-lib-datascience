a9a72ae99ffec2d68908018aeafa584ee79c1bf5
Amirhosein <amirhoseinmohammadisabet@gmail.com>
2022-05-26 11:29:02 +0430

    matplotlib learning

--- matplotlib-learning.py
+++ matplotlib-learning.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-#Heatmap means color map
+# Heatmap means color map
 '''
 language = 'python', 'java', 'PHP', 'HTML', 'JavaScript', 'C++'
 popularity = [22.2, 17, 15.5, 8, 9, 3]
@@ -12,7 +11,7 @@ plt.pie(popularity, explode= explode, labels=language, colors=colors, autopct='%
 plt.show()
 '''
 
-#plot sinus
+# plot sinus
 """
 # make data
 x = np.linspace(0, 10, 100)
@@ -28,7 +27,7 @@ ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
 plt.show()
 """
 
-#scatter plot
+# scatter plot
 """
 np.random.seed(3)
 x = 4 + np.random.normal(0, 2, 24)
@@ -47,8 +46,8 @@ ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
 
 plt.show()
 """
-#bar plot random
-
+# bar plot random
+"""
 # make data:
 np.random.seed(3)
 x = 0.5 + np.arange(8)
@@ -61,4 +60,69 @@ ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
 
 ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
        ylim=(0, 8), yticks=np.arange(1, 8))
+plt.show()
+"""
+
+# stem plot random values
+"""
+# make data
+np.random.seed(3)
+x = 0.5 + np.arange(8)
+y = np.random.uniform(2, 7, len(x))
+
+# plot
+fig, ax = plt.subplots()
+
+ax.stem(x, y)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+plt.show()
+"""
+
+# step plot random values
+"""
+# make data
+np.random.seed(5)
+x = 0.5 + np.arange(8)
+y = np.random.uniform(2, 7, len(x))
+
+# plot
+fig, ax = plt.subplots()
+
+ax.step(x, y, linewidth=2.5)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+plt.show()
+"""
+
+# plot fill between random values
+"""
+# make data
+np.random.seed(1)
+x = np.linspace(0, 8, 30)
+y1 = 2 + 4*x/8 + np.random.uniform(0.0, 0.5, len(x))
+y2 = 1 + 2*x/8 + np.random.uniform(0.0, 0.5, len(x))
+
+# plot
+fig, ax = plt.subplots()
+
+ax.fill_between(x, y1, y2, alpha=.5, linewidth=0)
+ax.plot(x, (y1 + y2)/2, linewidth=2)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+plt.show()
+"""
+
+
+# make data
+X, Y = np.meshgrid(np.linspace(-3, 3, 16), np.linspace(-3, 3, 16))
+Z = (1 - X/2 + X**5 + Y**3) * np.exp(-X**2 - Y**2)
+
+# plot
+fig, ax = plt.subplots()
+
+ax.imshow(Z)
 plt.show()
