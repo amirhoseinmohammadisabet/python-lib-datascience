99ae21e08fef2f3ae3e96480919f2375f01b0cc1
Amirhosein <amirhoseinmohammadisabet@gmail.com>
2022-05-26 11:09:49 +0430

    matplotlib learning

--- matplotlib-learning.py
+++ matplotlib-learning.py
@@ -1,16 +1,64 @@
 import matplotlib.pyplot as plt
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import re
+import numpy as np
+
 
 #Heatmap means color map
+'''
 language = 'python', 'java', 'PHP', 'HTML', 'JavaScript', 'C++'
 popularity = [22.2, 17, 15.5, 8, 9, 3]
 colors = ["#1f77b4", "#ff7f0e", "#2ca02c","blue", "red", "green"]
 explode = (0.1, 0,0,0,0,0)
 plt.pie(popularity, explode= explode, labels=language, colors=colors, autopct='%1.1f%%',shadow=True, startangle=140)
-#plt.show()
+plt.show()
+'''
 
+#plot sinus
+"""
+# make data
+x = np.linspace(0, 10, 100)
+y = 4 + 2 * np.sin(2 * x)
 
+# plot
+fig, ax = plt.subplots()
 
+ax.plot(x, y, linewidth=2.0)
 
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+plt.show()
+"""
+
+#scatter plot
+"""
+np.random.seed(3)
+x = 4 + np.random.normal(0, 2, 24)
+y = 4 + np.random.normal(0, 2, len(x))
+# size and color:
+sizes = np.random.uniform(15, 80, len(x))
+colors = np.random.uniform(15, 80, len(x))
+
+# plot
+fig, ax = plt.subplots()
+
+ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+plt.show()
+"""
+#bar plot random
+
+# make data:
+np.random.seed(3)
+x = 0.5 + np.arange(8)
+y = np.random.uniform(2, 7, len(x))
+
+# plot
+fig, ax = plt.subplots()
+
+ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+plt.show()
