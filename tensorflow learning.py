6eda1e76760dd74d34482bcab0d696654091c084
Amirhosein <amirhoseinmohammadisabet@gmail.com>
2022-05-27 16:05:43 +0430

    tensorflow learning

--- tensorflow learning.py
+++ tensorflow learning.py
@@ -41,12 +41,17 @@ print('rows:', row, 'columns:', col)
 
 import os
 
+
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
+import pandas as pd
 import numpy as np
 from keras import Sequential
 from keras.layers import Dense
+from keras.datasets import mnist
 
+"""
 celsius_q = np.array([-40, 10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 for i, c in enumerate(celsius_q):
@@ -58,3 +63,23 @@ model.compile(loss='mean_square_error',
               optimizer=tf.keras.optimizers.Adam(0.1))
 history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
 print("Training is finished")
+"""
+
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
