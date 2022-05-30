4a08ee0e6024d2c06bb5bf8c792bbaa32f4dbc84
Amirhosein <amirhoseinmohammadisabet@gmail.com>
2022-05-30 14:09:16 +0430

    proceeding tensor advanced

--- tensor-advanced.py
+++ tensor-advanced.py
@@ -1,8 +1,15 @@
 import os
-import pprint
-import tempfile
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-from typing import Dict, Text
+import sys
 
-import numpy as np
+# Disable tensorflow compilation warnings
+import matplotlib.pyplot as plt
+import plotly.express as px
+from PIL import Image
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
+
+im = Image.open(r"Man.jpg")
+print(im)
+im.show()
+
+
